dd6cbb1d991edc369b046b5d0c129c9803c63326
Stejin Stephen <stejinstephen@gmail.com>
2023-10-07 16:49:39 +0530

    app.get /books

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,31 @@ app.post('/books', async (req, res) => {
     }
 })
 
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({})
+        return res.status(201).json({
+            count: books.length,
+            data: books
+        });
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: err.message })
+    }
+});
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id)
+
+        return res.status(200).json(book);
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: err.message })
+    }
+});
+
 mongoose
     .connect(mongodbURL)
     .then(() => {
